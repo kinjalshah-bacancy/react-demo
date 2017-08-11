b71c5534ceb5f8652bca862298c209c130b680ac
bacancy <info@bacancytechnology.com>
2017-08-11 18:15:41 +0530

    add propvalidation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import logo from './logo.svg';
 import './App.css';
 
@@ -25,7 +26,8 @@ class App extends Component {
         {this.props.header}
         {this.props.content}
 
-        <Clock />*/}
+      */}
+        <Clock />
       </div>
     );
   }
@@ -41,21 +43,19 @@ class Clock extends Component{
     super(props);
 
     this.state = {
-      date : new Date(),
-      comment : "Hello"
+      date : new Date()
     }
   }
 
   componentDidMount(){
     this.timeId = setInterval(
-    () => {date : this.tick()}
+    () => {this.tick()}
     ,1000)
   }
 
   tick(){
     this.setState({
-      date : new Date(),
-      comment : this.state.comment + "o"
+      date : new Date()
     });
   }
 
@@ -67,7 +67,6 @@ class Clock extends Component{
     return(
       <div>
         <h2>Current Tims is : {this.state.date.toLocaleTimeString()}</h2>
-        <p>{this.state.comment}</p>
       </div>
     );
   }
@@ -115,28 +114,62 @@ class Content extends Component{
       color : 'blue'
     };
     return(
-      <p className="App-intro">
-        <h1>My page starts here! </h1>
-        <h2> start with JSX </h2>
-        To get started, edit <code>src/App.js</code> and save to reload.
-        <h4 style={myStyle}> Hello World </h4>
-        <h5>{2 + 6} </h5>
-        <h5>{ i == 5 ? "true" : "false"}</h5>
-        <h3>{this.props.contect}</h3>
-        <table>
-          <thead>
-            <th>
-              <td>ID</td>
-              <td>Name</td>
-              <td>Age</td>
-            </th>
-          </thead>
-          <tbody>
-            {this.state.data.map((person, i) => <TableRow key={i} data={person}/> )}
-          </tbody>
-        </table>
-      </p>
+      <div>
+        <div className="App-intro">
+          <h1>My page starts here! </h1>
+          <h2> start with JSX </h2>
+          To get started, edit <code>src/App.js</code> and save to reload.
+          <h4 style={myStyle}> Hello World </h4>
+          <h5>{2 + 6} </h5>
+          <h5>{ i === 5 ? "true" : "false"}</h5>
+          <h3>{this.props.contect}</h3>
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Age</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.data.map((person, i) => <TableRow key={i} data={person}/> )}
+            </tbody>
+          </table>
+        </div>
+        <div>
+          <h5>{this.props.propArray}</h5>
+          <h5>{this.props.propBool}</h5>
+          <h5>{this.props.propFunc(5)}</h5>
+          <h5>{this.props.propNumber}</h5>
+          <h5>{this.props.propString}</h5>
+          <h5>{this.props.propObject.obj1}</h5>
+          <h5>{this.props.propObject.obj2}</h5>
+          <h5>{this.props.propObject.obj3}</h5>
+        </div>
+      </div>
     );
+  }
+}
+
+Content.propTypes = {
+ propArray: PropTypes.array.isRequired,
+ propBool: PropTypes.bool.isRequired,
+ propFunc: PropTypes.func,
+ propNumber: PropTypes.number,
+ propString: PropTypes.string,
+ propObject: PropTypes.object
+}
+
+Content.defaultProps = {
+  propArray : [1,2,3,4],
+  propBool : true,
+  propFunc : function(e) { return (e)},
+  propNumber : 1,
+  propString : "Hello",
+  propObject : {
+    "obj1" : "val1",
+    "obj2" : "val2",
+    "obj3" : "val3"
   }
 }
 
