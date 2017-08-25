bf0b843c1c6bc54b06b83c9623ae525886760ba6
bacancy <info@bacancytechnology.com>
2017-08-25 13:45:36 +0530

    set componets

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import logo from './logo.svg';
 import './App.css';
@@ -16,18 +17,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {
-          /*<h1>{this.state.headerText}</h1>
-          <h1>{this.state.contentText}</h1>*/
-        }
         <Header header={this.state.headerText}/>
         <Content contect={this.state.contentText}/>
-        {/*
-        {this.props.header}
-        {this.props.content}
-
-      */}
-        <Clock />
       </div>
     );
   }
@@ -38,39 +29,6 @@ App.defaultProps = {
   content: "default props from content"
 }
 
-class Clock extends Component{
-  constructor(props){
-    super(props);
-
-    this.state = {
-      date : new Date()
-    }
-  }
-
-  componentDidMount(){
-    this.timeId = setInterval(
-    () => {this.tick()}
-    ,1000)
-  }
-
-  tick(){
-    this.setState({
-      date : new Date()
-    });
-  }
-
-  componetWillUnmount(){
-    clearInterval(this.timeId)
-  }
-
-  render(){
-    return(
-      <div>
-        <h2>Current Tims is : {this.state.date.toLocaleTimeString()}</h2>
-      </div>
-    );
-  }
-}
 
 class Header extends Component{
   render(){
@@ -87,100 +45,41 @@ class Content extends Component{
   constructor(props){
     super(props);
 
-    this.state = {
-      data : [
-        {
-          "id" : 1,
-          "name" : "ABC",
-          "age" : 20
-        },
-        {
-          "id" : 2,
-          "name" : "PQR",
-          "age" : 30
-        },
-        {
-          "id" : 3,
-          "name" : "XYZ",
-          "age" : 40
-        }
-      ]
-    }
-  }
-
-  render(){
-    var i = 5;
-    var myStyle = {
-      color : 'blue'
+    this.state ={
+      data: [],
+      count: 0,
     };
-    return(
-      <div>
-        <div className="App-intro">
-          <h1>My page starts here! </h1>
-          <h2> start with JSX </h2>
-          To get started, edit <code>src/App.js</code> and save to reload.
-          <h4 style={myStyle}> Hello World </h4>
-          <h5>{2 + 6} </h5>
-          <h5>{ i === 5 ? "true" : "false"}</h5>
-          <h3>{this.props.contect}</h3>
-          <table>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Age</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.data.map((person, i) => <TableRow key={i} data={person}/> )}
-            </tbody>
-          </table>
-        </div>
-        <div>
-          <h5>{this.props.propArray}</h5>
-          <h5>{this.props.propBool}</h5>
-          <h5>{this.props.propFunc(5)}</h5>
-          <h5>{this.props.propNumber}</h5>
-          <h5>{this.props.propString}</h5>
-          <h5>{this.props.propObject.obj1}</h5>
-          <h5>{this.props.propObject.obj2}</h5>
-          <h5>{this.props.propObject.obj3}</h5>
-        </div>
-      </div>
-    );
+    this.updateMyState = this.updateMyState.bind(this);
+    this.updateRandom = this.updateRandom.bind(this);
+    this.findDom = this.findDom.bind(this);
   }
-}
-
-Content.propTypes = {
- propArray: PropTypes.array.isRequired,
- propBool: PropTypes.bool.isRequired,
- propFunc: PropTypes.func,
- propNumber: PropTypes.number,
- propString: PropTypes.string,
- propObject: PropTypes.object
-}
-
-Content.defaultProps = {
-  propArray : [1,2,3,4],
-  propBool : true,
-  propFunc : function(e) { return (e)},
-  propNumber : 1,
-  propString : "Hello",
-  propObject : {
-    "obj1" : "val1",
-    "obj2" : "val2",
-    "obj3" : "val3"
+  updateRandom(){
+    this.forceUpdate();
   }
-}
+  findDom(){
+    var myDiv = Document.getElementById('myDiv');
+    ReactDOM.findDOMNode(myDiv).style.color = 'blue';
+  }
+  updateMyState(){
+    var count = this.state.count;
+    count++;
+    var item = "click -"+count;
+    var my_array = this.state.data;
+    my_array.push(item);
+    this.setState({data: my_array,count: count})
+  }
 
-class TableRow extends Component{
   render(){
     return(
-      <tr>
-        <td>{this.props.data.id}</td>
-        <td>{this.props.data.name}</td>
-        <td>{this.props.data.age}</td>
-      </tr>
+      <div className="App-intro">
+        <h1>Componet API Start</h1>
+        <button onClick={this.updateMyState}>Click Me</button>
+        <h4>State Data: {this.state.data}</h4>
+        <button onClick={this.updateRandom}>Random No.</button>
+        <h4>Rendom Number: {Math.random()}</h4>
+        <button onClick={this.findDom}>Find Dom.</button>
+        <h4 id="myDiv">this is div</h4>
+      </div>
     );
   }
 }
