0bd30d99f56950e92ed62e7ae68e9292b1b6ba57
bacancy <info@bacancytechnology.com>
2017-09-04 19:04:06 +0530

    add routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import logo from './logo.svg';
 import './App.css';
+import { Link } from 'react-router'
+
 
 class App extends Component {
   constructor(props){
@@ -16,173 +18,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {
-          /*<h1>{this.state.headerText}</h1>
-          <h1>{this.state.contentText}</h1>*/
-        }
-        <Header header={this.state.headerText}/>
-        <Content contect={this.state.contentText}/>
-        {/*
-        {this.props.header}
-        {this.props.content}
-
-      */}
-        <Clock />
+        <ul>
+          <li><Link to="/home">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contect</Link></li>
+          <li><Link to="/blog">Blog</Link></li>
+        </ul>
+        {this.props.children}
       </div>
     );
   }
 }
 
-App.defaultProps = {
-  header : "default props from header",
-  content: "default props from content"
-}
 
-class Clock extends Component{
-  constructor(props){
-    super(props);
-
-    this.state = {
-      date : new Date()
-    }
-  }
-
-  componentDidMount(){
-    this.timeId = setInterval(
-    () => {this.tick()}
-    ,1000)
-  }
-
-  tick(){
-    this.setState({
-      date : new Date()
-    });
-  }
-
-  componetWillUnmount(){
-    clearInterval(this.timeId)
-  }
-
-  render(){
-    return(
-      <div>
-        <h2>Current Tims is : {this.state.date.toLocaleTimeString()}</h2>
-      </div>
-    );
-  }
-}
-
-class Header extends Component{
-  render(){
-    return(
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-    );
-  }
-}
-
-class Content extends Component{
-  constructor(props){
-    super(props);
-
-    this.state = {
-      data : [
-        {
-          "id" : 1,
-          "name" : "ABC",
-          "age" : 20
-        },
-        {
-          "id" : 2,
-          "name" : "PQR",
-          "age" : 30
-        },
-        {
-          "id" : 3,
-          "name" : "XYZ",
-          "age" : 40
-        }
-      ]
-    }
-  }
-
-  render(){
-    var i = 5;
-    var myStyle = {
-      color : 'blue'
-    };
-    return(
-      <div>
-        <div className="App-intro">
-          <h1>My page starts here! </h1>
-          <h2> start with JSX </h2>
-          To get started, edit <code>src/App.js</code> and save to reload.
-          <h4 style={myStyle}> Hello World </h4>
-          <h5>{2 + 6} </h5>
-          <h5>{ i === 5 ? "true" : "false"}</h5>
-          <h3>{this.props.contect}</h3>
-          <table>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Age</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.data.map((person, i) => <TableRow key={i} data={person}/> )}
-            </tbody>
-          </table>
-        </div>
-        <div>
-          <h5>{this.props.propArray}</h5>
-          <h5>{this.props.propBool}</h5>
-          <h5>{this.props.propFunc(5)}</h5>
-          <h5>{this.props.propNumber}</h5>
-          <h5>{this.props.propString}</h5>
-          <h5>{this.props.propObject.obj1}</h5>
-          <h5>{this.props.propObject.obj2}</h5>
-          <h5>{this.props.propObject.obj3}</h5>
-        </div>
-      </div>
-    );
-  }
-}
-
-Content.propTypes = {
- propArray: PropTypes.array.isRequired,
- propBool: PropTypes.bool.isRequired,
- propFunc: PropTypes.func,
- propNumber: PropTypes.number,
- propString: PropTypes.string,
- propObject: PropTypes.object
-}
-
-Content.defaultProps = {
-  propArray : [1,2,3,4],
-  propBool : true,
-  propFunc : function(e) { return (e)},
-  propNumber : 1,
-  propString : "Hello",
-  propObject : {
-    "obj1" : "val1",
-    "obj2" : "val2",
-    "obj3" : "val3"
-  }
-}
-
-class TableRow extends Component{
-  render(){
-    return(
-      <tr>
-        <td>{this.props.data.id}</td>
-        <td>{this.props.data.name}</td>
-        <td>{this.props.data.age}</td>
-      </tr>
-    );
-  }
-}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import Blog from './Blog';
 import registerServiceWorker from './registerServiceWorker';
+import { Router, Route, Link, browserHistory } from 'react-router'
 
-ReactDOM.render(<App/>, document.getElementById('root'));
-registerServiceWorker();
+
+ReactDOM.render(
+	<Router history={browserHistory} >
+		<Route path='/' component={App}>
+			<Route path='/home' component={Home} />
+			<Route path='/about' component={About} />
+			<Route path='/contact' component={Contact} />
+			<Route path='/blog' component={Blog} />
+		</Route>
+	</Router>
+	,document.getElementById('root')
+);
+
